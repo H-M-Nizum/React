20c3f69c03d1db7cdd086c40a7f7e9cf23ff87ef
H-M-Nizum <hmnizum1714032@gmail.com>
2024-03-24 22:34:02 +0600

    Load api and display country details

--- countries_api/src/App.js
+++ countries_api/src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountiesAPI></LoadCountiesAPI>
     </div>
   );
 }
 
+
+function LoadCountiesAPI(){
+  const [countries, setCountries] = useState([])
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setCountries(data)
+    })
+  },[])
+
+  return (
+    <div>
+      <h1>Load and Display All countries in the world!!!</h1>
+      <div>
+        <h2>Avabile Countries : {countries.length}</h2>
+        <div className='cardContainer'>
+          {
+            countries.map(country => DisplayCountries(country))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function DisplayCountries(props){
+  const {name, region, population, capital, timezones} = props
+  // console.log(Object.values(languages))
+  return (
+    <div className='card'>
+      <div>
+        <img src="{props.flags.png}" alt="" />
+        {/* <img src="" alt="" /> */}
+      </div>
+      <h3>Name : {name.common}</h3>
+      <hr />
+      <p>Region : {region}</p>
+      <p>Population : {population}</p>
+     
+      {/* <p>Language : {languages.ell}</p> */}
+      <p>Capital : {capital}</p>
+      <p>TimeZones : {timezones}</p>
+    </div>
+  )
+}
 export default App;
